1a200f7b13159f7cf6488303d210628205faf554
Sam Dixon <sam.dixon@berkeley.edu>
2019-06-05 13:37:07 -0700

    update with equivalent widths, better smoothing window calculation

--- spectral_lines/gauss.py
+++ spectral_lines/gauss.py
@@ -8,20 +8,36 @@ class Gauss(Measure):
     def __init__(self, spectrum, line='SiII6355', interp_grid=0.1, sim=False, norm=True):
         super(Gauss, self).__init__(spectrum, line, interp_grid, sim, norm)
 
-    def get_smoothed_feature_spec(self, return_popt=False):
-        w, f, e = self.wave_feat, self.flux_feat, self.var_feat
+    def get_interp_feature_spec(self, return_popt=False):
+        w, f, e = self.wave_subfeat, self.flux_subfeat, self.var_subfeat
         g = lambda x, *p: p[0]+p[1]*x+p[2]*np.exp(-(x-p[3])**2/(2*p[4]**2))
         p0 = [1., -0.1, -0.5, np.mean(w), 50.]
         popt, pcov = curve_fit(g, w, f, p0=p0, sigma=e)
-        w_s = np.arange(np.min(w), np.max(w), self.interp_grid)
+        w_s = np.arange(np.min(self.wave_feat), np.max(self.wave_feat), self.interp_grid)
         if return_popt:
             return w_s, g(w_s, *popt), popt
         return w_s, g(w_s, *popt)
-
+    
+    def get_pseudo_continuum(self):
+        w_s, g, popt = self.get_interp_feature_spec(return_popt=True)
+        line = lambda x, *p: p[0] + p[1]*x
+        return line(w_s, *popt[:2])
+    
     def get_line_velocity(self):
-        w, f, popt = self.get_smoothed_feature_spec(return_popt=True)
-        w_abs = w[np.where(f == np.min(f))][0]
+        """
+        Find the velocity of the line (defined to be the velocity that Doppler
+        shifts l_0 to the minimum of the smoothed, interpolated spectrum)
+        """
+        w, f = self.get_interp_feature_spec()
+        f_pc = self.get_pseudo_continuum()
+        f /= f_pc
+        search_range = (w >= self.l_brange[-1]) & (w <= self.l_rrange[0])
+        min_f = np.min(f[search_range])
+        w_abs = w[np.where(f == min_f)][0]
         v_abs = self.vel_space(w_abs)
-        w_em = w[np.where(f == np.max(f))][0]
-        v_em = self.vel_space(w_em)
-        return v_abs, v_em
+        return v_abs
+    
+    def get_equiv_width(self):
+        _, _, popt = self.get_interp_feature_spec(return_popt=True)
+        return popt[2]*np.sqrt(2*np.pi/popt[4]**2)
+        
--- spectral_lines/spline.py
+++ spectral_lines/spline.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 class Spl(Measure):
 
     def __init__(self, spectrum, line='SiII6355', interp_grid=0.1, sim=False,
-                 norm='SNID', n_l=30, smooth_fac=0.005):
+                 norm='SNID', n_l=16, smooth_fac=0.005):
         super(Spl, self).__init__(spectrum, line, interp_grid, sim, norm)
         self.n_l = n_l
         self.smooth_fac = smooth_fac
@@ -41,6 +41,24 @@ class Spl(Measure):
             return w_int, f_int, spl
         else:
             return w_int, f_int
+        
+    def get_pseudo_continuum(self, return_extrema=False):
+        """Returns the line connecting the ends of the feature
+        """
+        w, f = self.get_interp_feature_spec()
+        bw_cut = (w >= self.l_brange[0]) & (w <= self.l_brange[1])
+        rw_cut = (w >= self.l_rrange[0]) & (w <= self.l_rrange[1])
+        max_b_ind = np.where(f[bw_cut]==max(f[bw_cut]))[0]
+        max_r_ind = np.where(f[rw_cut]==max(f[rw_cut]))[0]
+        fr = f[rw_cut][max_r_ind]
+        fb = f[bw_cut][max_b_ind]
+        wr = w[rw_cut][max_r_ind]
+        wb = w[bw_cut][max_b_ind]
+        slope = (fr-fb)/(wr-wb)
+        inter = f[rw_cut][max_r_ind] - slope*w[rw_cut][max_r_ind]
+        if return_extrema:
+            return inter + slope * w, [wb, wr, fb, fr]
+        return inter + slope * w
 
     def get_line_velocity(self):
         """
@@ -48,8 +66,20 @@ class Spl(Measure):
         shifts l_0 to the minimum of the smoothed, interpolated spectrum)
         """
         w, f = self.get_interp_feature_spec()
-        w_abs = w[np.where(f == np.min(f))][0]
+        f_cont = self.get_pseudo_continuum()
+        f /= f_cont
+        search_range = (w >= self.l_brange[-1]) & (w <= self.l_rrange[0])
+        min_f = np.min(f[search_range])
+        w_abs = w[np.where(f == min_f)][0]
         v_abs = self.vel_space(w_abs)
-        w_em = w[np.where(f == np.max(f))][0]
-        v_em = self.vel_space(w_em)
-        return v_abs, v_em
+        return v_abs
+
+    def get_equiv_width(self):
+        w, f = self.get_interp_feature_spec()
+        f_c, extrema = self.get_pseudo_continuum(return_extrema=True)
+        wb, wr, _, _ = extrema
+        integrand = 1 - f/f_c
+        dl = np.diff(w)
+        wb_ind = np.where(w==wb)[0][0]
+        wr_ind = np.where(w==wr)[0][0]
+        return np.sum(np.dot(integrand[wb_ind:wr_ind], dl[wb_ind:wr_ind]))
